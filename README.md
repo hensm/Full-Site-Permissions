587f129a92dc471b11320ecc6b2660d397580c99
Matt F <marcii@cssu.in>
2015-12-09 22:54:26 +0000

    Add to git

---
+++ README.md
@@ -0,0 +1,2 @@
+#Full Site Permissions
+Shows all permissions for a site in the identity popup
---
+++ index.js
@@ -0,0 +1,51 @@
+const { Cu				} = require("chrome");
+const { viewFor			} = require("sdk/view/core");
+const { browserWindows	} = require("sdk/windows");
+const { SitePermissions	} = Cu.import("resource:///modules/SitePermissions.jsm");
+
+let orig;
+
+function replace(browserWindow) {
+	console.log(browserWindow);
+	let xulWindow = viewFor(browserWindow).window;
+
+	if (!orig) {
+		orig = xulWindow.gIdentityHandler.updateSitePermissions;
+	}
+
+	xulWindow.gIdentityHandler.updateSitePermissions = function () {
+		while (this._permissionList.hasChildNodes())
+			this._permissionList.removeChild(this._permissionList.lastChild);
+
+		let uri = xulWindow.gBrowser.currentURI;
+
+		for (let permission of SitePermissions.listPermissions()) {
+			let state = SitePermissions.get(uri, permission);
+
+			/*if (state == SitePermissions.UNKNOWN)
+			continue;*/
+
+			let item = this._createPermissionItem(permission, state);
+			this._permissionList.appendChild(item);
+		}
+	};
+}
+
+function restore(browserWindow) {
+	let xulWindow = viewFor(browserWindow).window;
+
+	xulWindow.gIdentityHandler.updateSitePermissions = orig;
+}
+
+browserWindows.on("open", replace);
+
+exports.main = function(options, callbacks) {
+	for (let browserWindow of browserWindows) {
+		replace(browserWindow);
+	}
+}
+exports.onUnload = function() {
+	for (let browserWindow of browserWindows) {
+		restore(browserWindow);
+	}
+}
